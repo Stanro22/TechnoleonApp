201c05304d4af7c48706347a57788c2eb05149a1
Stanro22 <stanroozendaal@hotmail.com>
2021-12-02 17:06:21 +0100

    Login

--- TechnoleonApp/ContentView.swift
+++ TechnoleonApp/ContentView.swift
@@ -8,62 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    init() {
-        //UITabBar.appearance().backgroundColor = UIColor(red: 0.15, green: 0.21, blue: 0.40, alpha: 1.00)
-        //UITabBar.appearance().unselectedItemTintColor = UIColor(Color.white)
-        //UITabBarItem.appearance().setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: .selected)
-    }
+    @State var email: String = ""
+    @State var password: String = ""
     
     var body: some View {
         NavigationView{
             VStack{
-                Text("Overzicht")
-                HStack(alignment: .bottom){
-                        NavigationLink(destination: ContentView()) {
-                            VStack{
-                                Image(systemName: "binoculars.fill")
-                                    .foregroundColor(Color.white)
-                                Text("Mijn overzicht")
-                                    .foregroundColor(Color.white)
-                                    .font(.custom("", size: 14))
-                            }
-                            .frame(width: 100, height: 20)
-                            .padding(EdgeInsets(top: 20, leading: 15, bottom: 20, trailing: 15))
-                            .background(Color(red: 0.18, green: 0.25, blue: 0.44))
+                HStack{
+                    Image(systemName: "star")
+                    Image(systemName: "star")
+                }
+                .padding()
+                HStack{
+                    TextField("Email adres", text: $email)
+                        .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 0))
+                        .background(Color(red: 0.95, green: 0.95, blue: 0.95))
+                        .frame(width: 300, height: 30)
+                        .overlay(Rectangle().frame(width: nil, height: 1, alignment: .bottom).padding(EdgeInsets(top: 10, leading: 0, bottom: -5, trailing: 0)).foregroundColor(Color(red: 0.90, green: 0.31, blue: 0.11)), alignment: .bottom)
+                }
+                .padding()
+                
+                HStack{
+                    SecureField("Password", text: $password)
+                        .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 0))
+                        .background(Color(red: 0.95, green: 0.95, blue: 0.95))
+                        .frame(width: 300, height: 30)
+                        .overlay(Rectangle().frame(width: nil, height: 1, alignment: .bottom).padding(EdgeInsets(top: 10, leading: 0, bottom: -5, trailing: 0)).foregroundColor(Color(red: 0.90, green: 0.31, blue: 0.11)), alignment: .bottom)
+                }
+                .padding()
+                
+                HStack{
+                    NavigationLink(destination: OverviewView()) {
+                        Button(action: login){
+                            Text("Inloggen")
+                                .font(.custom("", size: 22))
                         }
-                        .padding(EdgeInsets(top: 550, leading: 0, bottom: 0, trailing: -10))
-                        NavigationLink(destination: TeamSelectionView()) {
-                            VStack{
-                                Image(systemName: "globe")
-                                    .foregroundColor(Color.white)
-                                Text("Testen")
-                                    .foregroundColor(Color.white)
-                                    .font(.custom("", size: 14))
-                            }
-                            .frame(width: 100, height: 20)
-                            .padding(EdgeInsets(top: 20, leading: 15, bottom: 20, trailing: 15))
-                            .background(Color(red: 0.15, green: 0.21, blue: 0.40))
-                        }
-                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: -10))
-                        NavigationLink(destination: ProfileView()) {
-                            VStack{
-                                Image(systemName: "person.circle.fill")
-                                    .foregroundColor(Color.white)
-                                Text("Profiel")
-                                    .foregroundColor(Color.white)
-                                    .font(.custom("", size: 14))
-                            }
-                            .frame(width: 100, height: 20)
-                            .padding(EdgeInsets(top: 20, leading: 15, bottom: 20, trailing: 15))
-                            .background(Color(red: 0.15, green: 0.21, blue: 0.40))
-                        }
-                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        .foregroundColor(Color.white)
+                        .frame(width: 250, height: 20)
+                        .padding()
+                        .background(Color(red: 0.90, green: 0.31, blue: 0.11))
+                    }.cornerRadius(10)
+                        .padding(EdgeInsets(top: 100, leading: 0, bottom: 50, trailing: 0))
                 }
             }
-            .navigationTitle("Overzicht")
-            .navigationBarColor(UIColor(red: 0.15, green: 0.21, blue: 0.40, alpha: 1.00))            
         }
+        .navigationTitle("Inloggen")
+    }
+    
+    func login(){
+        
     }
 }
 
