ab51db549289d4bdf929fdd0b83f95a5d8c2a409
Stanro22 <stanroozendaal@hotmail.com>
2021-12-03 20:15:21 +0100

    Layout

--- TechnoleonApp/TeamOverviewView.swift
+++ TechnoleonApp/TeamOverviewView.swift
@@ -1,87 +1,58 @@
 //
-//  OverviewView.swift
+//  TeamOverviewView.swift
 //  TechnoleonApp
 //
-//  Created by user206981 on 12/2/21.
+//  Created by user206981 on 12/3/21.
 //
 
 import SwiftUI
 
-struct OverviewView: View {
+struct TeamOverviewView: View {
     @State var expand = false
     
     var body: some View {
         VStack{
             HStack{
-                Image(systemName: "star")
-                    .resizable()
-                VStack(alignment: .leading){
-                    Text("Naam:")
-                    Text("Club:")
-                    VStack {
-                        HStack {
-                            Text("Zeeburgia 1")
-                                .foregroundColor(Color.black)
-                                .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 50))
-                            Image(systemName: expand ? "chevron.up" : "chevron.down")
-                                .resizable()
-                                .frame(width: 13, height: 6, alignment: .trailing)
-                                .foregroundColor(Color(red: 0.90, green: 0.31, blue: 0.11))
-                        }
-                    }
-                    .padding()
-                    .frame(width: 200, height: 40)
-                    .background(Color(red: 0.93, green: 0.93, blue: 0.93))
-                    .overlay(Divider().background(Color(red: 0.90, green: 0.31, blue: 0.11)), alignment: .bottom)
-                    .onTapGesture {
-                        self.expand.toggle()
-                    }
-                    if expand {
-                        //menu items here
+                VStack{
+                    HStack {
+                        Text("Geen Test")
+                            .font(.custom("", size: 14))
+                            .foregroundColor(Color.black)
+                            .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 50))
+                        Image(systemName: expand ? "chevron.up" : "chevron.down")
+                            .resizable()
+                            .frame(width: 13, height: 6, alignment: .trailing)
+                            .foregroundColor(Color(red: 0.90, green: 0.31, blue: 0.11))
                     }
                 }
-            }
-            .frame(width: 300, height: 80)
-            .padding()
-            .foregroundColor(Color.white)
-            .background(Color(red: 0.62, green: 0.65, blue: 0.90))
-            .cornerRadius(20)
-            
-            HStack{
+                .padding()
+                .frame(width: 170, height: 40)
+                .background(Color(red: 0.93, green: 0.93, blue: 0.93))
+                .onTapGesture {
+                    self.expand.toggle()
+                }
+                if expand {
+                    //menu items here
+                }
+                
                 NavigationLink(destination: ContentView()) {
                     VStack{
-                        Text("Laatste Resultaten")
+                        Text("Speler")
+                            .foregroundColor(Color.white)
+                            .font(.custom("", size: 14))
+                        Text("Vergelijken")
                             .foregroundColor(Color.white)
                             .font(.custom("", size: 14))
                     }
-                    .frame(width: 120, height: 20)
-                    .padding(EdgeInsets(top: 12, leading: 15, bottom: 12, trailing: 15))
+                    .frame(width: 70, height: 20)
+                    .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
                     .background(Color(red: 0.18, green: 0.25, blue: 0.44))
                 }
-                .cornerRadius(15)
-                .padding(EdgeInsets(top: 5, leading: 0, bottom: 5, trailing: 0))
-                    
-                    NavigationLink(destination: ContentView()) {
-                        VStack{
-                            Text("Team Overzicht")
-                                .foregroundColor(Color.white)
-                                .font(.custom("", size: 14))
-                        }
-                        .frame(width: 120, height: 20)
-                        .padding(EdgeInsets(top: 12, leading: 15, bottom: 12, trailing: 15))
-                        .background(Color(red: 0.18, green: 0.25, blue: 0.44))
-                    }
-                    .cornerRadius(15)
+                .cornerRadius(10)
+                .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 0))
             }
-            
-            HStack{
-                VStack(alignment: .leading){
-                    Text("Beste Resultaten")
-                        .padding(EdgeInsets(top: -80, leading: -150, bottom: 12, trailing: 15))
-                }
-            }
-            .frame(width: 300, height: 150)
-            .padding()
+            .frame(width: 300, height: 450, alignment: .top)
+            .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
             .foregroundColor(Color.white)
             .background(Color(red: 0.62, green: 0.65, blue: 0.90))
             .cornerRadius(20)
@@ -99,7 +70,7 @@ struct OverviewView: View {
                         .padding(EdgeInsets(top: 20, leading: 15, bottom: 20, trailing: 15))
                         .background(Color(red: 0.18, green: 0.25, blue: 0.44))
                     }
-                    .padding(EdgeInsets(top: 150, leading: 0, bottom: 0, trailing: -10))
+                    .padding(EdgeInsets(top: 50, leading: 0, bottom: 0, trailing: -10))
                     NavigationLink(destination: TeamSelectionView()) {
                         VStack{
                             Image(systemName: "globe")
@@ -128,13 +99,13 @@ struct OverviewView: View {
                     .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
             }
         }
-        .navigationTitle("Overzicht")
+        .navigationTitle("Team Overzicht")
         .navigationBarColor(UIColor(red: 0.15, green: 0.21, blue: 0.40, alpha: 1.00))
     }
 }
 
-struct OverviewView_Previews: PreviewProvider {
+struct TeamOverviewView_Previews: PreviewProvider {
     static var previews: some View {
-        OverviewView()
+        TeamOverviewView()
     }
 }
