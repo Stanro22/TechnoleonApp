451f16702e46922be925a7dc8a0a727a9c962495
Stanro22 <stanroozendaal@hotmail.com>
2021-12-07 20:27:13 +0100

    KTK3 test

---
+++ TechnoleonApp/KTK3Request.swift
@@ -0,0 +1,26 @@
+//
+//  KTK3Request.swift
+//  TechnoleonApp
+//
+//  Created by user206503 on 12/7/21.
+//
+
+import Foundation
+
+struct KtK3Request: Encodable{
+    let hoppingSideways1: Int?
+    let hoppingSideways2: Int?
+    let movingSideways1: Int?
+    let movingSideways2: Int?
+    let handEye1: Int?
+    let handEye2: Int?
+    let beam11: Int?
+    let beam12: Int?
+    let beam13: Int?
+    let beam21: Int?
+    let beam22: Int?
+    let beam23: Int?
+    let beam31: Int?
+    let beam32: Int?
+    let beam33: Int?
+}
