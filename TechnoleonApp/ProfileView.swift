f27588ae3534ccd417af120235c4595dd59daf64
Stanro22 <stanroozendaal@hotmail.com>
2021-12-15 12:12:00 +0100

    Profile layout

--- TechnoleonApp/ProfileView.swift
+++ TechnoleonApp/ProfileView.swift
@@ -9,11 +9,87 @@ import SwiftUI
 
 struct ProfileView: View {
     @ObservedObject var technoleonAPI = TechnoleonAPI.shared
+    @ObservedObject var loggedInUser = LoggedInUser.shared
     
     var body: some View {
         VStack{
-            Text("Profiel")
-                .padding(EdgeInsets(top: 0, leading: 15, bottom: 20, trailing: 15))
+            VStack{
+                Image(systemName: "person")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .padding()
+                    .background(Color.gray)
+                if loggedInUser.name != nil {
+                    Text(loggedInUser.name!)
+                        .font(.custom("", size: 20))
+                        .foregroundColor(Color.white)
+                        .padding(EdgeInsets(top: 20, leading: 15, bottom: 0, trailing: 15))
+                }
+                else{
+                    Text("Jan de Groot")
+                        .font(.custom("", size: 20))
+                        .foregroundColor(Color.white)
+                        .padding(EdgeInsets(top: 20, leading: 15, bottom: 0, trailing: 15))
+                }
+                
+            }
+            .frame(width: 375, height: 200)
+            .background(Color(UIColor(red: 0.15, green: 0.21, blue: 0.40, alpha: 0.90)))
+            
+            HStack{
+                NavigationLink(destination: OverviewView()) {
+                    VStack{
+                        Text("Doelen")
+                            .foregroundColor(Color.white)
+                            .font(.custom("", size: 14))
+                    }
+                    .frame(width: 100, height: 20)
+                    .padding(EdgeInsets(top: 20, leading: 15, bottom: 20, trailing: 15))
+                    .background(Color.gray)
+                }
+                .cornerRadius(10)
+                .padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))
+                NavigationLink(destination: OverviewView()) {
+                    VStack{
+                        Text("Teams")
+                            .foregroundColor(Color.white)
+                            .font(.custom("", size: 14))
+                    }
+                    .frame(width: 100, height: 20)
+                    .padding(EdgeInsets(top: 20, leading: 15, bottom: 20, trailing: 15))
+                    .background(Color(red: 0.38, green: 0.44, blue: 0.64))
+                }
+                .cornerRadius(10)
+                .padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: 0))
+            }
+            .padding()
+                        
+            VStack(alignment: .leading){
+                HStack{
+                    Image(systemName: "star")
+                    Text("Emailadres")
+                    Text("")
+                }
+                .overlay(Divider().background(Color(red: 0.90, green: 0.31, blue: 0.11)), alignment: .bottom)
+                
+                HStack{
+                    Image(systemName: "star")
+                    Text("Geboortejaar")
+                    Text("")
+                }
+                .overlay(Divider().background(Color(red: 0.90, green: 0.31, blue: 0.11)), alignment: .bottom)
+                
+                HStack{
+                    Image(systemName: "star")
+                    Text("Gender")
+                    Text("")
+                }
+                .overlay(Divider().background(Color(red: 0.90, green: 0.31, blue: 0.11)), alignment: .bottom)
+            }
+            .padding()
+            .frame(width: 300, height: 120)
+            .background(Color(UIColor(red: 0.90, green: 0.90, blue: 0.90, alpha: 0.80)))
+            
             
             Button(action: logout){
                 Text("Logout")
@@ -33,7 +109,7 @@ struct ProfileView: View {
                         .padding(EdgeInsets(top: 20, leading: 15, bottom: 20, trailing: 15))
                         .background(Color(red: 0.15, green: 0.21, blue: 0.40))
                     }
-                    .padding(EdgeInsets(top: 400, leading: 0, bottom: 0, trailing: -10))
+                    .padding(EdgeInsets(top: 20, leading: 0, bottom: 0, trailing: -10))
                     NavigationLink(destination: TeamSelectionView()) {
                         VStack{
                             Image(systemName: "globe")
