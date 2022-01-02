8a96ac9ac5ad4b6efdb9355f4f22eb912eabc0c1
Stanro22 <stanroozendaal@hotmail.com>
2022-01-02 15:35:37 +0100

    All test layout

--- TechnoleonApp/FSTView.swift
+++ TechnoleonApp/FSTView.swift
@@ -8,8 +8,109 @@
 import SwiftUI
 
 struct FSTView: View {
+    @ObservedObject var timerManager = StopwatchManager()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Spacer()
+            VStack{
+                HStack{
+                    Text("Slalom")
+                    Spacer()
+                    Text("00.00")
+                }
+                .frame(width: 200, height: 30)
+                .padding(EdgeInsets(top: -5, leading: 0, bottom: 0, trailing: 0))
+                .overlay(Rectangle().frame(width: 190, height: 2, alignment: .bottom).padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)).foregroundColor(Color(red: 0.38, green: 0.44, blue: 0.64)), alignment: .bottom)
+                HStack{
+                    Text("Dribbel")
+                    Spacer()
+                    Text("00.00")
+                }
+                .frame(width: 200, height: 30)
+                .padding(EdgeInsets(top: -5, leading: 0, bottom: 0, trailing: 0))
+                .overlay(Rectangle().frame(width: 190, height: 2, alignment: .bottom).padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)).foregroundColor(Color(red: 0.38, green: 0.44, blue: 0.64)), alignment: .bottom)
+                HStack{
+                    Text("Achterwaarts")
+                    Spacer()
+                    Text("00.00")
+                }
+                .frame(width: 200, height: 30)
+                .padding(EdgeInsets(top: -5, leading: 0, bottom: 0, trailing: 0))
+                .overlay(Rectangle().frame(width: 190, height: 2, alignment: .bottom).padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)).foregroundColor(Color(red: 0.38, green: 0.44, blue: 0.64)), alignment: .bottom)
+                HStack{
+                    Text("8-figuur")
+                    Spacer()
+                    Text("00.00")
+                }
+                .frame(width: 200, height: 30)
+                .padding(EdgeInsets(top: -5, leading: 0, bottom: 0, trailing: 0))
+                .overlay(Rectangle().frame(width: 190, height: 2, alignment: .bottom).padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)).foregroundColor(Color(red: 0.38, green: 0.44, blue: 0.64)), alignment: .bottom)
+                HStack{
+                    Text("Passen")
+                    Spacer()
+                    Text("00.00")
+                }
+                .frame(width: 200, height: 30)
+                .padding(EdgeInsets(top: -5, leading: 0, bottom: 0, trailing: 0))
+                .overlay(Rectangle().frame(width: 230, height: 4, alignment: .bottom).padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)).foregroundColor(Color(red: 0.38, green: 0.44, blue: 0.64)), alignment: .bottom)
+                HStack{
+                    Text("Eindtijd")
+                    Spacer()
+                    Text("00.00")
+                }
+                .frame(width: 200, height: 30)
+                .padding(EdgeInsets(top: -5, leading: 0, bottom: 0, trailing: 0))
+            }
+            Spacer()
+            
+            Button(action: timerManager.reset){
+                Text("Reset")
+                    .foregroundColor(Color.white)
+                    .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
+                    .background(Color(red: 0.73, green: 0.05, blue: 0.05))
+            }
+            .cornerRadius(15)
+            
+            Text(secondsToMinutesAndSeconds(seconds: timerManager.seconds))
+                .font(.custom("", size: 40))
+                .foregroundColor(Color(red: 0.90, green: 0.31, blue: 0.11))
+                .frame(width: 180, height: 180)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 90)
+                        .stroke(lineWidth: 3)
+                        .foregroundColor(Color(red: 0.15, green: 0.21, blue: 0.40))
+                )
+                .onAppear(){
+                    self.timerManager.seconds = 0
+                }
+                
+            Image(systemName: timerManager.timerMode == .running ? "pause.circle.fill" : "play.circle.fill")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50, height: 50)
+                .padding(EdgeInsets(top: -20, leading: 0, bottom: 0, trailing: 0))
+                .foregroundColor(Color(red: 0.15, green: 0.21, blue: 0.40))
+                .onTapGesture(perform: {
+                    if self.timerManager.timerMode == .initial {
+                        self.timerManager.seconds = 0
+                    }
+                    self.timerManager.timerMode == .running ? self.timerManager.pause() : self.timerManager.start()
+                })
+            Spacer()
+            
+            NavigationLink(destination: EndOfTestView().onAppear{ }) {
+                Text("Sla gegevens op")
+                    .font(.custom("", size: 22))
+                    .foregroundColor(Color.white)
+                    .frame(width: 250, height: 20)
+                    .padding()
+                    .background(Color(red: 0.90, green: 0.31, blue: 0.11))
+            }.cornerRadius(10)
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 5, trailing: 0))
+        }
+        .navigationTitle("FST")
+        .navigationBarColor(UIColor(red: 0.15, green: 0.21, blue: 0.40, alpha: 1.00))
     }
 }
 
--- TechnoleonApp/GiTView.swift
+++ TechnoleonApp/GiTView.swift
@@ -9,7 +9,74 @@ import SwiftUI
 
 struct GiTView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Spacer()
+            VStack{
+                HStack{
+                    Button(action: {}){
+                        Text("1")
+                            .font(.custom("", size: 60))
+                            .foregroundColor(Color.white)
+                    }
+                    .frame(width: 100, height: 130)
+                    .background(Color(red: 0.38, green: 0.44, blue: 0.64))
+                    Button(action: {}){
+                        Text("2")
+                            .font(.custom("", size: 60))
+                            .foregroundColor(Color.white)
+                    }
+                    .frame(width: 100, height: 130)
+                    .background(Color(red: 0.38, green: 0.44, blue: 0.64))
+                    Button(action: {}){
+                        Text("3")
+                            .font(.custom("", size: 60))
+                            .foregroundColor(Color.white)
+                    }
+                    .frame(width: 100, height: 130)
+                    .background(Color(red: 0.38, green: 0.44, blue: 0.64))
+                }
+                HStack{
+                    Button(action: {}){
+                        Text("4")
+                            .font(.custom("", size: 60))
+                            .foregroundColor(Color.white)
+                    }
+                    .frame(width: 100, height: 130)
+                    .background(Color(red: 0.38, green: 0.44, blue: 0.64))
+                    Button(action: {}){
+                        Text("5")
+                            .font(.custom("", size: 60))
+                            .foregroundColor(Color.white)
+                    }
+                    .frame(width: 100, height: 130)
+                    .background(Color(red: 0.38, green: 0.44, blue: 0.64))
+                    Button(action: {}){
+                        Text("6")
+                            .font(.custom("", size: 60))
+                            .foregroundColor(Color.white)
+                    }
+                    .frame(width: 100, height: 130)
+                    .background(Color(red: 0.38, green: 0.44, blue: 0.64))
+                }
+            }
+            Text("1")
+                .font(.custom("", size: 50))
+            + Text("/10")
+                .font(.custom("", size: 50))
+            Spacer()
+            
+            NavigationLink(destination: EndOfTestView().onAppear{ }) {
+                Text("Volgende ronde")
+                    .font(.custom("", size: 22))
+                    .foregroundColor(Color.white)
+                    .frame(width: 250, height: 20)
+                    .padding()
+                    .background(Color(red: 0.38, green: 0.44, blue: 0.64))
+            }.cornerRadius(10)
+                .padding(EdgeInsets(top: 0, leading: 0, bottom: 5, trailing: 0))
+        }
+        .navigationTitle("GiT")
+        .navigationBarColor(UIColor(red: 0.15, green: 0.21, blue: 0.40, alpha: 1.00))
     }
 }
 
