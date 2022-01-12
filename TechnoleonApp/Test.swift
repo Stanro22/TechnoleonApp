ae88b3a0d06e3b913e727020f836c84017211271
Stanro22 <stanroozendaal@hotmail.com>
2022-01-12 12:00:32 +0100

    Fixes

--- TechnoleonApp/Test.swift
+++ TechnoleonApp/Test.swift
@@ -11,22 +11,39 @@ struct Test: Decodable, Hashable{
     let playerId: String?
     let category: String?
     let lsptTestId: String?
+    let lsptTest: LSPTTest?
     let ktK3plusTestId: String?
+    let ktk3PlusTest: KTK3Test?
     let giTTestId: String?
+    let giTTest: GiTTest?
     let phvTestId: String?
+    let phvTest: PHVTest?
     let sitAndReachTestId: String?
+    let sitAndReachTest: SitAndReachTest?
     let fatPercentageTestId: String?
+    let fatPercentageTest: FatPercentageTest?
     let ijtBallControllTestId: String?
+    //let ijtTest: IJTTest?
     let fstTestId: String?
+    let fstTest: FSTTest?
     let insaitJoyTestId: String?
+    let insaitJoyTest: InsaitJoyTest?
     let intervalShuttleRunTestId: String?
+    let intervalShuttleRunTest: IntervalShuttleRunTest?
     let tvpS3TestId: String?
+    //let tvpS3Test: TVPS3Test?
     let yoYoStaminaTestId: String?
+    let yoYoStaminaTest: YoYoTest?
     let oneLegStanceTestId: String?
+    let oneLegStanceTest: OneLegStanceTest?
     let tTestTestId: String?
+    let tTestTest: TTestTest?
     let vfmtTestId: String?
+    //let vfmtTest: VFMTTest?
     let tenx5MSprintTestId: String?
+    let tenx5MSprintTest: Sprint10x5Test?
     let tenTwentyThirtyFiveSprintId: String?
+    let tenTwentyThirtyFiveSprint: TenTwentyThirtyFiveSprintTest?
     let id: String?
     let created: String?
     let modified: String?
---
+++ TechnoleonApp/TestBody/TenTwentyThirtyFiveSprintTest.swift
@@ -0,0 +1,16 @@
+//
+//  TenTwentyThirtyFiveSprintTest.swift
+//  TechnoleonApp
+//
+//  Created by user206503 on 1/12/22.
+//
+
+import Foundation
+
+struct TenTwentyThirtyFiveSprintTest: Decodable, Hashable{
+    var distanceInM: Int? = 0
+    var seconds: String? = ""
+    var id: String? = ""
+    var createdAt: String = "" //format to compare
+    var modifiedAt: String = "" //format to compare
+}
